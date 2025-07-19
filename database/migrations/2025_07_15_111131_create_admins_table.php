0e909ca098aa44a596db0a42778990ea5e5206a3
krishh2001 <161043749+krishh2001@users.noreply.github.com>
2025-07-19 15:50:24 +0530

    new

--- database/migrations/2025_07_15_111131_create_admins_table.php
+++ database/migrations/2025_07_15_111131_create_admins_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         $table->string('email')->unique();
         $table->string('password');
         $table->rememberToken();
-        $table->timestamps();
+        $table->timestamps(); 
     });
 }
 
